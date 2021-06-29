b6172ab3af02cb245c16ace2365b8eeb0e6c99a7
Shuaib <shuaib.salar.ahmed@gmail.com>
2021-06-29 16:03:50 -0500

    Added ECDF

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import numpy as np 
+import matplotlib.pyplot as plt
 
 class PandasComputations:
 
@@ -28,6 +30,18 @@ class PandasComputations:
             
             final_count = count
             list_of_cases.append(final_count)
+
+
+    def ECDF(self, column_name, x_label, y_label):
+        x = np.sort(self.dataframe[column_name])
+        y = np.arange(1, len(x)+1) / len(x)
+        _ = plt.plot(x,y, marker='.', linestyle='none')
+        _ = plt.xlabel(x_label)
+        _ = plt.ylabel(y_label)
+        plt.margins(0.02)
+        plt.show()
+
+
 
 
         
