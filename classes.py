2e3c0f48f9f06a837a892c9ab69844b952fafe39
Shuaib <shuaib.salar.ahmed@gmail.com>
2021-06-29 19:03:06 -0500

    Added 'interesting_metrics_to_compute'

--- classes.py
+++ classes.py
@@ -9,12 +9,23 @@ class PandasComputations:
     def __init__(self, data_frame):
         self.dataframe = data_frame
 
+    
+    def interesting_metrics_to_compute(self):
+        """Prints interesting metrics to compute"""
+        print("ECDF")
+        print("")
+        print("CDF")
+        print("")
+        print("PDF")
+        
+
 
 
     def check_df(self):
         print(self.dataframe.head())
 
     def cumulative_subtraction_across_column(self, column_name):
+        """Title is self-explantory"""
         list_of_cases = []
         series_items = list(self.dataframe[column_name])
         
@@ -32,14 +43,17 @@ class PandasComputations:
             list_of_cases.append(final_count)
 
 
-    def ECDF(self, column_name, x_label, y_label):
+    def ECDF(self, column_name, x_label):
+        """Computes a graph that shows the percentage of data that has that specific value"""
         x = np.sort(self.dataframe[column_name])
         y = np.arange(1, len(x)+1) / len(x)
         _ = plt.plot(x,y, marker='.', linestyle='none')
         _ = plt.xlabel(x_label)
-        _ = plt.ylabel(y_label)
+        _ = plt.ylabel("ECDF")
         plt.margins(0.02)
         plt.show()
+
+    
 
 
 
